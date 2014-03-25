490edbcfaaf937783f7fc795d299205b18b51f4b
kiat-mao <kiat.mao@gmail.com>
2014-03-26 04:27:13 +0800

    merge routes from master

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 ShpostStorage::Application.routes.draw do
 
-<<<<<<< HEAD
   resources :businesses
 
-=======
->>>>>>> e8985c6f80b4d0e211260628e6002cbeccf850ef
   resources :stock_logs, only: [:index, :show]
 
   resources :commodities
