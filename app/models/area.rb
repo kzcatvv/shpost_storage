7266606e36fa9e9f59a41dc38888014ea06d8c59
Tony.Gao <kzcatvv@gmail.com>
2014-04-01 15:09:08 +0800

    fix bug for area

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,5 +1,6 @@
 class Area < ActiveRecord::Base
 	belongs_to :storage
+	has_many :shelves, dependent: :destroy
 
-	validates_presence_of :desc, :area_code, :message => '不能为空字符'
+	validates_presence_of :desc, :area_code, :storage_id, :message => '不能为空字符'
 end
