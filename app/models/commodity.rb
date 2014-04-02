c0e7ee0f3c5ad36d2025f7580cafd3ea31bbac20
Tony.Gao <kzcatvv@gmail.com>
2014-04-02 11:41:31 +0800

    fix bug

--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -1,6 +1,6 @@
 class Commodity < ActiveRecord::Base
 	belongs_to :unit
-	belongs_to :gooodstype
+	belongs_to :goodstype
 	validates_presence_of :cno, :name, :message => '不能为空'
 	validates_uniqueness_of :cno, :message => '该商品已存在'
 end
