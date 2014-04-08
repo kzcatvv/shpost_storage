b6c9e24ca671934355e69e77fca600939092958e
LiuYingYing <anniliu.student@sina.com>
2014-04-08 10:02:12 +0800

    some changes

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,6 +1,6 @@
 class Purchase < ActiveRecord::Base
 	belongs_to :unit
-	belongs_to :bussiness
+	belongs_to :business
 	has_many :purchase_detail, dependent: :destroy
 	        
 	STATUS = { no: '未处理', yes: '处理中', prepare: '发货' ,recevie: '已收货'}
