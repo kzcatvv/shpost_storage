8771534f95981b205672ff9c890526a63794eff8
Tony.Gao <kzcatvv@gmail.com>
2014-04-23 16:01:02 +0800

    fix bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -115,7 +115,7 @@ function modify(current)
       if(data.success){
         // alert(data.responseText)
         var jsonData = eval("("+data.responseText+")");
-        $("td#stock_logs_actamount_"+param[3]).text(jsonData.actual_amount);
+        $("p#stock_logs_actamount_"+param[3]).text(jsonData.actual_amount);
         if (jsonData.actual_amount < $("p#stock_logs_amount_"+param[3]).text() && jsonData.operation_type == "out") {
           $("p#stock_logs_amount_"+param[3]).css("background-color","red");
         } else {
