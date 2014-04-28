674c7e5b02f5a8b982daf6f09f72aebff078409a
rr <rongr881@163.com>
2014-04-28 13:49:04 +0800

    bcm_interface 4/28 13:50

--- app/controllers/bcm_interface_controller.rb
+++ app/controllers/bcm_interface_controller.rb
@@ -4,23 +4,85 @@ class BcmInterfaceController < ApplicationController
 	before_filter :verify_sign
 
     def commodity_enter
-    # supplier_no = @context_hash["SUPPLIER"]
-    sku = @context_hash["SKU"]
-    return render json: error_builder('0005', '商品sku编码为空') if sku.blank?
-    # spec = @context_hash["SPEC"]
-    name = @context_hash["NAME"]
-    return render json: error_builder('0005', '商品名称为空') if name.blank?
 
-    # desc = @context_hash["DESC"]
+    	@plaintext["goodsInfoS"].each do |goodsInfo|
+    		context_hash=Hash.new
+    		context_hash["SUPPLIER"]=goodsInfo["vendorId"]
+    		context_hash["SKU"]=goodsInfo["prodId"]
+    		context_hash["NAME"]=goodsInfo["prodName"]
+    		context_hash["SPEC"]=goodsInfo["prodSpecs"]
+    		context_hash["DESC"]=""
 
-    thirdpartcode = StandardInterface.commodity_enter(@context_hash, @business, @unit)
+    		sku = context_hash["SKU"]
+    		return render json: error_builder('0005', '商品sku编码为空') if sku.blank?
 
-    if !thirdpartcode.blank?
-      render json: success_builder({"SKU" => thirdpartcode.specification.sku })
-    else
-      render json: error_builder('9999')
+    		name = context_hash["NAME"]
+    		return render json: error_builder('0005', '商品名称为空') if name.blank?
+
+    		thirdpartcode = StandardInterface.commodity_enter(context_hash, @business, @unit)
+    		if !thirdpartcode.blank?
+    			render json: success_builder({"SKU" => thirdpartcode.specification.sku })
+    		else
+    			render json: error_builder('9999')
+    		end
+    	end
     end
-  end
+
+    def order_enter
+    	context_hash=Hash.new
+    	context_hash["ORDER_ID"]=@plaintext["orderId"]
+    	context_hash["TRANS_SN"]=@plaintext["transSN"]
+    	context_hash["DATE"]=""
+    	context_hash["EXPS"]=""
+    	context_hash["CUST_NAME"]=@plaintext["name"]
+    	context_hash["PROVINCE"],context_hash["CITY"],context_hash["COUNTY"]=@plaintext["address"].split(",")
+    	context_hash["MOBILE"]=@plaintext["mobile"]
+    	context_hash["TEL"]=@plaintext["mobile"]
+    	context_hash["ADDR"]=@plaintext["address"]
+    	context_hash["zip"]=@plaintext["mobile"]
+    	context_hash["DESC"]=@plaintext["orderMem"]
+    	context_hash["CUST_UNIT"]=""
+    	context_hash["QTY_SUM"]=""
+    	context_hash["AMT_SUM"]=""
+    	context_hash["EXPS_SUM"]=""
+    	context_hash["SEND_ADDR"]=""
+    	context_hash["SEND_NAME"]=""
+    	context_hash["SEND_ZIP"]=""
+    	context_hash["SEND_MOBILE"]=""
+
+        context_hash["ORDER_DETAILS"]=Array.new
+    	@plaintext["prodS"].each_with_index do |prod,i|
+    		context_hash["ORDER_DETAILS"][i]=Hash.new
+            context_hash["ORDER_DETAILS"][i]["DELIVER_NO"]=prod["clubdeliverNo"]
+            context_hash["ORDER_DETAILS"][i]["SUPPLIER"]=@plaintext["vendorId"]
+            context_hash["ORDER_DETAILS"][i]["SKU"]=prod["prodId"]
+            context_hash["ORDER_DETAILS"][i]["SPEC"]=prod["prodSpec"]
+            context_hash["ORDER_DETAILS"][i]["DESC"]=""
+            context_hash["ORDER_DETAILS"][i]["NAME"]=prod["prodName"]
+            context_hash["ORDER_DETAILS"][i]["QTY"]=prod["prodAmt"]
+            context_hash["ORDER_DETAILS"][i]["PRICE"]=""
+            context_hash["ORDER_DETAILS"][i]["AMT"]=""
+    	end
+        order_id = context_hash['ORDER_ID']
+        return render json: error_builder('0005', '订单号为空') if order_id.blank?
+        trans_sn = context_hash['TRANS_SN']
+        return render json: error_builder('0005', '交易流水号为空') if trans_sn.blank?
+        cust_name = context_hash['CUST_NAME']
+        return render json: error_builder('0005', '收件人姓名为空') if cust_name.blank?
+        addr = context_hash['ADDR']
+        return render json: error_builder('0005', '收货人地址为空') if addr.blank?
+
+        order_details = context_hash['ORDER_DETAILS']
+        return render json: error_builder('0005', '商品列表为空') if order_details.blank?
+
+        order = StandardInterface.order_enter(context_hash, @business, @unit)
+
+        if !order.blank?
+            render json: success_builder({'ORDER_NO' => order.no })
+        else
+            render json: error_builder('9999')
+        end
+    end
 
 	private
 	def verify_params
@@ -29,7 +91,7 @@ class BcmInterfaceController < ApplicationController
 
 		@context = params[:plaintext]
 		begin
-			@context_hash = ActiveSupport::JSON.decode(@context)
+			@plaintext = ActiveSupport::JSON.decode(@context)
 		rescue ActiveSupport::JSON.parse_error
 			return render json: error_builder('0002')
 		end
--- spec/controllers/bcm_interface_controller_spec.rb
+++ spec/controllers/bcm_interface_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe BcmInterfaceController do
+	describe "access without login" do
+		context "commodity_enter" do
+			before :each do
+				@format = 'JSON'
+                @business = Business.find_by(no: '0003')
+                #@unit = Unit.find_by(no: '0001')
+                plaintext={"transSn"=>"201211051643571000316", "goodsInfoS"=>[{"vendorId"=>"00001017", "prodId"=>"10000079", "prodName"=>"第一个商品", "prodSpecs"=>"第一个商品的商品规格"}]}
+                context_hash=Hash.new
+    		    context_hash["SUPPLIER"]=plaintext["goodsInfoS"]["vendorId"]
+    		    context_hash["SKU"]=plaintext["goodsInfoS"]["prodId"]
+    		    context_hash["NAME"]=plaintext["goodsInfoS"]["prodName"]
+    		    context_hash["SPEC"]=plaintext["goodsInfoS"]["prodSpecs"]
+    		    context_hash["DESC"]=""
+                @context = context_hash.to_json
+                @sign = Digest::MD5.base64digest(@context + @business.secret_key)
+            end
 
+            it "should change Thirdpartcode " do
+                expect{get :commodity_enter, format: @format, context: @context, business: '0003', unit: '0001', sign: @sign}.to change(Thirdpartcode, :count).by(1)
+            end
+
+            it "should success " do
+                get :commodity_enter, format: @format, context: @context, business: '0003', unit: '0001', sign: @sign
+                response_hash = ActiveSupport::JSON.decode(response.body)
+                # expect(response.body).to eq '{"FLAG":"success"}'
+                expect(response_hash["returnFlag"]).to eq "00"
+            end
+        end
+    end
 end
