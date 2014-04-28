a07ba75b1abb65e8edb9f780441f973b57638a66
LiuYingYing <anniliu.student@sina.com>
2014-04-28 15:59:27 +0800

    20140428005

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,13 +11,13 @@ module ApplicationHelper
 
     def span_commodity_select
         content_tag(:span) do
-       	 concat select_tag("ajax_commodity_id", @commodities,{:prompt => "请选择"})
+       	concat select_tag("ajax_commodity_id", @commodities,{:prompt => "请选择"})
        end
     end
 
     def span_specification_select(obj_id)
-    	content_tag(:span) do
-       	 concat select(obj_id.to_sym,"specification_id", " ",{:prompt => "请选择"})
+    	  content_tag(:span) do
+       	concat select(obj_id.to_sym,"specification_id", " ",{:prompt => "请选择"})
        end
     end
 end
--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,6 @@
 class Business < ActiveRecord::Base
 	belongs_to :unit
 	validates_presence_of :name
+  validates_uniqueness_of :no, scope: :unit_id,:message => "商户已存在"
+
 end
