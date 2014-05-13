54cde101b4f3b01c7347f8752215526520694923
leopp <leopeng1983@gmail.com>
2014-05-13 15:09:17 +0800

    add relationship_contact

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513054559) do
+ActiveRecord::Schema.define(version: 20140513062226) do
 
   create_table "areas", force: true do |t|
     t.integer  "storage_id"
@@ -197,6 +197,11 @@ ActiveRecord::Schema.define(version: 20140513054559) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "spec_desc"
+  end
+
+  create_table "relationships_contacts", id: false, force: true do |t|
+    t.integer "relationship_id"
+    t.integer "contact_id"
   end
 
   create_table "roles", force: true do |t|
