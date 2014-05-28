5b42a1dd527bac5251ed40575583cea52e6beddf
LiuYingYing <anniliu.student@sina.com>
2014-05-28 11:20:11 +0800

    20140528001

--- app/controllers/area_shelf_controller.rb
+++ app/controllers/area_shelf_controller.rb
@@ -37,7 +37,7 @@ class AreaShelfController < ApplicationController
     @shelf = Shelf.new(shelf_params)
     @shelf.shelf_code = setShelfCode(shelf_params)
 
-    # @shelf.shelf_code = @areas.find(shelf_params[:area_id]).area_code
+   # @shelf.shelf_code = @areas.find(shelf_params[:area_id]).area_code
     # @shelf.shelf_code << "-" << change(shelf_params[:area_length])
     # @shelf.shelf_code << "-" << change(shelf_params[:area_width])
     # @shelf.shelf_code << "-" << change(shelf_params[:area_height])
