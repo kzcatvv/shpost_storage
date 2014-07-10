bde809699993923503c30ae93d76d0e4810694e0
Tony.Gao <kzcatvv@gmail.com>
2014-07-10 01:13:12 -0700

    gix bug for tracking no

--- app/controllers/print_controller.rb
+++ app/controllers/print_controller.rb
@@ -47,23 +47,78 @@ class PrintController < ApplicationController
 			@keycorder = Keyclientorder.create(keyclient_name: "auto",unit_id: current_user.unit_id,storage_id: session[:current_storage].id,batch_id: batch_id,user: current_user,status: "printed")
         end
 		@transport_type=params[:transport_type]
-		regular = /([\D]*)([\d]*)/
-		start=regular.match(params[:start])
-		numberSize = start[2].size
-	    ending=regular.match(params[:end])
+		#regular = /([\D]*)([\d]*)/
+		start=getTrackingNumber(@transport_type, params[:start])
+		numberSize = start[1].size
+	    ending=getTrackingNumber(@transport_type, params[:end])
 		@ids=params[:id].split(",").map(&:to_i)
-		(start[2].to_i..ending[2].to_i).each_with_index do |num,i|
+		(start[1].to_i..ending[1].to_i).each_with_index do |num,i|
 			order = Order.find(@ids[i])
-			order.tracking_number=start[1] << sprintf("%0#{numberSize.to_s}d", num)
+			num_s = sprintf("%0#{numberSize.to_s}d", num)
+			case num_s.size
+			when 8
+				order.tracking_number=start[0] << num_s << checkTrackingNO(num_s).to_s << start[2]
+			when 11
+				order.tracking_number=start[0] << num_s
+			end
 			order.status='printed'
 			order.user_id=current_user.id
 			order.transport_type=@transport_type
 			if params[:flag]=='filter'
 				order.keyclientorder_id=@keycorder.id
 			end
+			puts order.tracking_number
 			order.save
 		end
 		flash[:alert] = "正在打印"
+	end
+
+	def getTrackingNumber(transport_type, tracking_number)
+		return_no = []
+		case transport_type
+		when "tcsd"
+			case tracking_number.size
+			when 13
+				return_no << tracking_number[0,2] << tracking_number[2,8] << tracking_number[11,2]
+			when 10
+				return_no << tracking_number[0,2] << tracking_number[2,8] << "31"
+			when 8
+				return_no << "PN" << tracking_number << "31"
+			end
+		when "gnxb"
+			case tracking_number.size
+			when 13
+				return_no << tracking_number[0,2] << tracking_number[2,11] << ""
+			end
+		when "ems"
+			case tracking_number.size
+			when 13
+				return_no << tracking_number[0,2] << tracking_number[2,8] << tracking_number[11,2]
+			when 10
+				return_no << tracking_number[0,2] << tracking_number[2,8] << "06"
+			when 8
+				return_no << "10" << tracking_number << "06"
+			end
+		end
+		return return_no	
+	end
+
+	def checkTrackingNO(num)
+		x = [8,6,4,2,3,5,9,7]
+		num_a = num.split("")
+		sum = 0
+		num_a.each_with_index do |s,i|
+			sum = sum + s.to_i* x[i]
+		end
+		r = sum % 11
+		case r
+		when 0
+			return "5"
+		when 1
+			return "0"
+		else
+			return (11 - r).to_s
+		end
 	end
 
 end
