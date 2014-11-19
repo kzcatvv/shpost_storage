05d9ee11c022da94225786e3a2eb1f52173b8b81
leopp <leopeng1983@gmail.com>
2014-11-19 16:19:34 +0800

    mod orderreturn

---
+++ db/migrate/20141119015814_mod_column_to_orderreturndetail.rb
@@ -0,0 +1,9 @@
+class ModColumnToOrderreturndetail < ActiveRecord::Migration
+  def change
+  	remove_column :order_return_details, :integer
+  	remove_column :order_return_details, :string
+  	remove_column :order_return_details, :datetime
+  	change_column :order_return_details, :order_return_id, :integer
+  	change_column :order_return_details, :order_detail_id, :integer
+  end
+end
