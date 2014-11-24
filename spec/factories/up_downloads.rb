2740fa163d68a8ba840f7fcb28f97132d781def0
kiat-mao <kiat.mao@gmail.com>
2014-11-24 14:36:14 +0800

    add up_downloads

---
+++ spec/factories/up_downloads.rb
@@ -0,0 +1,12 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :up_download do
+    name "MyString"
+    use "MyString"
+    desc "MyString"
+    ver_no "MyString"
+    url "MyString"
+    oper_date "2014-11-19 11:02:12"
+  end
+end
