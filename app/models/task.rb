cb8e187a29fb6f05adc5c86ac6da8238546fd5fc
kiat-mao <kiat.mao@gmail.com>
2014-12-26 01:12:08 +0800

    mobile_interface update

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ActiveRecord::Base
   belongs_to :parent, polymorphic: true
   belongs_to :user
   belongs_to :storage
+
+  validates_uniqueness_of :barcode, :code
+
+  STATUS = {doing: 'doing', done: 'done'}
 end
