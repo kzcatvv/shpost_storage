e9861e3cec353c22fcd833830a0749f716439ca3
Tony.Gao <kzcatvv@gmail.com>
2015-01-14 09:37:40 +0800

    fix bug for specification all_name

--- app/models/specification.rb
+++ app/models/specification.rb
@@ -6,7 +6,14 @@ class Specification < ActiveRecord::Base
    validates_presence_of :commodity_id, :name, :message => '不能为空字符'
    # validates_uniqueness_of :sku, :message => '该商品已存在'
 
+  before_save :set_all_name
+
   def full_title
     "#{commodity.name} #{name}"
   end
+
+  def set_all_name
+    self.all_name = full_title
+  end
+
 end
