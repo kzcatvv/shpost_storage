7144f3ee2dc03795ef9d1b737395e1afbe4297f9
Tony.Gao <kzcatvv@gmail.com>
2015-01-19 14:55:12 +0800

    fix bug for migrate

--- db/migrate/20150109055935_add_barcode_to_relationships.rb
+++ db/migrate/20150109055935_add_barcode_to_relationships.rb
@@ -2,7 +2,8 @@ class AddBarcodeToRelationships < ActiveRecord::Migration
   def change
     add_column :relationships, :barcode, :string
     Relationship.find_each do |x|
-      x.save
+        next if x.unit.blank?
+	x.save
     end
   end
 end
