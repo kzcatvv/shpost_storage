6ed46d6502bf1589c2de36f37c984df8af13585f
kiat-mao <kiat.mao@gmail.com>
2015-01-20 16:51:27 +0800

    update unit order

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -6,7 +6,7 @@ class Unit < ActiveRecord::Base
   has_many :commodities, dependent: :destroy
   has_many :sequences, dependent: :destroy
 
-  validates_presence_of [:name, :short_name], :message => '不能为空'
+  validates_presence_of :name, :short_name, :message => '不能为空'
   validates_uniqueness_of :name, :message => '该单位已存在'
 
   validates_uniqueness_of :short_name, :message => '该缩写已存在'
--- app/views/units/show.json.jbuilder
+++ app/views/units/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @unit, :id, :name, :desc, :created_at, :updated_at
+json.extract! @unit, :id, :name, :desc, :created_at, :updated_at, :short_name
