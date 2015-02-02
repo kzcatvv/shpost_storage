21b474658e47e24e0d84f18c705b81705c4b75a0
Tony.Gao <kzcatvv@gmail.com>
2015-02-02 09:35:52 +0800

    add ttkd in orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -28,9 +28,9 @@ class Order < ActiveRecord::Base
   STATUS_SHOW = { waiting: '待处理', printed: '已打印', picking: '正在拣货', checked: '已审核', packed: '已包装', delivering: '正在寄送中', delivered: '已寄达', declined: '拒收', returned: '退回' }
 
 
-  TRANSPORT_TYPE= { gnxb: '国内小包', tcsd: '同城速递', ems: 'EMS'}
+  TRANSPORT_TYPE= { gnxb: '国内小包', tcsd: '同城速递', ems: 'EMS', ttkd: '天天快递'}
 
-  TRANSPORT_TYPE_print= {'国内小包'=>'gnxb', '同城速递'=>'tcsd', 'EMS'=>'ems'}
+  TRANSPORT_TYPE_print= {'国内小包'=>'gnxb', '同城速递'=>'tcsd', 'EMS'=>'ems', '天天快递'=>'ttkd'}
 
 
   SHORTAGE_TYPE = { yes: '是', no: '否' }
