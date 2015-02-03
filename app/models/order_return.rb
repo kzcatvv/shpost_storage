e7ffb9d7655ab1edf3411348b78cd68de8b81c9f
leopp <leopeng1983@gmail.com>
2015-02-03 16:38:26 +0800

    mod orderreturn check

--- app/models/order_return.rb
+++ app/models/order_return.rb
@@ -6,13 +6,29 @@ class OrderReturn < ActiveRecord::Base
   has_many :tasks, as: :parent
   has_many :user_logs, as: :parent
 
+  def all_checked?
+    self.stock_logs.each do |x|
+      if ! x.checked?
+        return false
+      end
+    end
+    return true
+  end
+
   def check!
     self.stock_logs.each do |x|
       x.check!
     end
 
-    self.order_return_details.each do |order_return_detail|
-      order_return_detail.return_in
+    if self.all_checked?
+
+      self.order_return_details.each do |order_return_detail|
+        order_return_detail.set_checked
+      end
+
+      self.update(status: "checked")
     end
   end
+
+
 end
