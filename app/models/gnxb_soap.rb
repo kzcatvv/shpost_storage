100da5d2c161a5b2ab6d7a50ab348894fd4c0665
Tony.Gao <kzcatvv@gmail.com>
2015-03-02 16:55:20 +0800

    fix bug for gnxb interface

--- app/models/gnxb_soap.rb
+++ app/models/gnxb_soap.rb
@@ -6,30 +6,54 @@ class GnxbSoap
     begin
       client = Savon.client(wsdl: uri, ssl_verify_mode: :none)
         
-      yjbhs = Order.where({status: [Order::STATUS[:delivering], Order::STATUS[:packed]], transport_type: "gnxb"}).map!{|x| x.tracking_number}
+      # yjbhs = Order.where({status: [Order::STATUS[:delivering], Order::STATUS[:packed]], transport_type: "gnxb"}).map!{|x| x.tracking_number}
+      orders = Order.where({status: [Order::STATUS[:delivering], Order::STATUS[:packed]], transport_type: "gnxb"})
 
       #yjbh = "9976613107785"
       #puts yjbhs
 
-      yjbhs.each do |yjbh|
+      orders.each do |order|
+        yjbh = order.tracking_number
         puts "-------------" << yjbh << "-----------------"
-        response = client.call(mehod.to_sym, message: { in0: "6", in1: "192.168.0.1", in2: yjbh })
-        json = response.body["#{mehod}_response".to_sym]["out".to_sym]
-        gnxb_info = ""
-        gnxb_status = Order::STATUS[:packed]
-        if json.blank?
-          next
+        # response = client.call(mehod.to_sym, message: { in0: "6", in1: "192.168.0.1", in2: yjbh })
+        # json = response.body["#{mehod}_response".to_sym]["out".to_sym]
+        # gnxb_info = ""
+        # gnxb_status = Order::STATUS[:packed]
+        # if json.blank?
+        #   next
+        # end
+        # json["mail".to_sym].each do |x|
+        #   action_time = convertToString(x[:action_date_time],Date)
+        #   office_desc = convertToString(x[:relation_office_desc],String)
+        #   office_name = convertToString(x[:office_name],String)
+        #   info_out = convertToString(x[:action_info_out],String)
+        #   gnxb_info << action_time << "#" << office_desc << "#" << office_name << "#" << info_out << "\n"
+        #   gnxb_status = returnStatus(info_out)
+        # end
+        gnxb_status = order.status
+        params = {}
+        params["mailNumber".to_sym] = yjbh
+        res = gnxb_post(uri, params)
+        if res.code.eql? '200'
+          body = JSON.parse(res.body)
+          code = body['code']
+          data = body['data']
+          mailTypeName = body['mailTypeName']
+          mailNumber = body['mailNumber']
+
+          if !code.eql? '-1'
+            gnxb_info = ""
+
+            data.each do |x|
+              time = x[0]
+              info = x[1]
+              gnxb_info << time << "#" << info << "\n"
+              gnxb_status = returnStatus(info)
+            end
+            updateOrder(yjbh,gnxb_info,gnxb_status)
+          end
         end
-        json["mail".to_sym].each do |x|
-          action_time = convertToString(x[:action_date_time],Date)
-          office_desc = convertToString(x[:relation_office_desc],String)
-          office_name = convertToString(x[:office_name],String)
-          info_out = convertToString(x[:action_info_out],String)
-          gnxb_info << action_time << "#" << office_desc << "#" << office_name << "#" << info_out << "\n"
-          gnxb_status = returnStatus(info_out)
-        end
-        updateOrder(yjbh,gnxb_info,gnxb_status)
-        sleep 5
+        # sleep 5
       end
     rescue Exception => e
       puts e
@@ -79,7 +103,7 @@ class GnxbSoap
   
   def returnStatus(tdxq)
     if !tdxq.blank?
-      if tdxq.start_with? '已签收' || tdxq.start_with? '已妥投'
+      if tdxq.end_with? '已签收' || tdxq.end_with? '已妥投'
         return Order::STATUS[:delivered]
       end
     end
