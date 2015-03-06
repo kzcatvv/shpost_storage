cd480015e66ee184dafc4bf6bdbbe5980d23a3fd
leopp <leopeng1983@gmail.com>
2015-03-06 09:57:25 +0800

    mod consumable db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303022949) do
+ActiveRecord::Schema.define(version: 20150303131803) do
 
   create_table "areas", force: true do |t|
     t.integer  "storage_id"
