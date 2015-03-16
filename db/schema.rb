8d9c24442d01a3aa2d49353dbeb87d9d500eb4fe
leopp <leopeng1983@gmail.com>
2015-03-16 10:55:18 +0800

    fix merge db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303131803) do
+ActiveRecord::Schema.define(version: 20150310030613) do
 
   create_table "areas", force: true do |t|
     t.integer  "storage_id"
@@ -321,9 +321,9 @@ ActiveRecord::Schema.define(version: 20150303131803) do
     t.integer  "unit_id"
     t.integer  "storage_id"
     t.integer  "keyclientorder_id"
-    t.string   "tracking_number"
     t.string   "province"
     t.string   "city"
+    t.string   "tracking_number"
     t.integer  "user_id"
     t.string   "is_shortage",                    default: "no"
     t.string   "business_order_id"
@@ -332,7 +332,7 @@ ActiveRecord::Schema.define(version: 20150303131803) do
     t.string   "pingan_ordertime"
     t.string   "pingan_operate"
     t.string   "customer_idnumber"
-    t.string   "tracking_info",     limit: 2000
+    t.string   "tracking_info",     limit: 4000
     t.string   "barcode"
     t.string   "batch_no"
     t.integer  "parent_id"
