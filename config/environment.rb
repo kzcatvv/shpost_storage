f85fd29d47f1b65f3404d465dec48b15dd87a642
leopp <leopeng1983@gmail.com>
2015-04-24 13:54:58 +0800

    add logistic url

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 ShpostStorage::Application.initialize!
+
+
+GETNUM_URL = 'http://222.66.108.206:51818/poe_server/programInterface.action'
