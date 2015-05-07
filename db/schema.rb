075be5446dedf24601d10ce8f93940a889316f88
leopp <leopeng1983@gmail.com>
2015-05-07 10:06:23 +0800

    mod stock out pick

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150506023339) do
+
   create_table "areas", force: true do |t|
     t.integer  "storage_id"
     t.string   "desc"
